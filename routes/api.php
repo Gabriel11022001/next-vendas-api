608d97a595fdd110671ee665e9ddad17f16faee4
Gabriel Rodrigues dos Santos <gabriel996567521@gmail.com>
2024-04-15 17:58:21 -0300

    adicionei os comentários indicando oque cada rota faz

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,17 @@ use App\Http\Controllers\CategoriaProdutoController;
 use App\Http\Controllers\SubCategoriaProdutoController;
 use Illuminate\Support\Facades\Route;
 
+// cadastrar categoria de produto
 Route::post('/categoria-produto', [ CategoriaProdutoController::class, 'cadastrarCategoriaProduto' ]);
+// cadastrar sub-categoria de produto
 Route::post('/sub-categoria', [ SubCategoriaProdutoController::class, 'cadastrarSubCategoriaProduto' ]);
+// atualizar status da categoria de produto
 Route::put('/categoria-produto/status/alterar', [ CategoriaProdutoController::class, 'alterarStatusCategoriaProduto' ]);
+// buscar todas as categorias
 Route::get('/categoria-produto', [ CategoriaProdutoController::class, 'listarTodasCategoriasProduto' ]);
+// buscar todas as sub-categorias
 Route::get('/sub-categoria', [ SubCategoriaProdutoController::class, 'listarTodasSubCategorias' ]);
+// buscar categoria pelo id
 Route::get('/categoria-produto/{idHash}', [ CategoriaProdutoController::class, 'buscarCategoriaProdutoPeloId' ]);
+// buscar sub-categoria pelo id
 Route::get('/sub-categoria/{idHash}', [ SubCategoriaProdutoController::class, 'buscarSubCategoriaPeloId' ]);
